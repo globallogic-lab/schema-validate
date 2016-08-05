9ea3102db9b1a9fd33fe7d803222d70ed6a93ee1
Oleh Kuchuk <oleh.kuchuk@globallogic.com>
2016-08-05 15:16:37 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const arrify = require('arrify')
+const Iterable = require('object-iterate')
+
+const isobject =
+  (value) =>
+    ( typeof value === 'object' && ({}).toString.call(value) === '[object Object]' )
+
+let Schema =
+  function (params) {
+    this.required = arrify(params.required)
+    this.data = params.data
+  }
+
+var proto = Schema.prototype
+
+proto.validate =
+  function (obj) {
+
+    const validatableProp =
+      (property) =>
+         property in this.data
+
+
+    const requiredProp =
+      (property) =>
+        ( this.required.indexOf(property) > -1 )
+
+
+    const removeItem =
+      (list, item) =>
+        list.splice(list.indexOf(item), 1)
+
+
+    const validateProperty =
+      (property, value) => {
+        if ( requiredProp(property) )
+          removeItem(this.required, property)
+
+        let handler = this.data[property]
+
+        if ( handler ) return handler(value)
+
+        return true
+      }
+
+
+    if ( !isobject(obj) )
+      throw new Error(`error while validating schema, value should be an object`)
+
+    let results = []
+
+    Iterable(obj)
+      .each(
+        (value, key, obj) => {
+          results.push(validateProperty(key, value, obj))
+        }
+      )
+
+    return ( ( results.every(res => res) ) && ( !this.required.length ) )
+  }
+
+module.exports = Schema
---
+++ test/index.js
@@ -0,0 +1,6 @@
+const Schema = require('../dist/index')
+const test = require('tape')
+
+test('init', (t) => {
+  t.pass()
+})
